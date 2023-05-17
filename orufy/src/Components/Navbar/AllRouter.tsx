8a10ede5a83fc09f770c7288b066a605100fc361
mdsa97087 <mdsa97087@gmail.com>
2023-05-17 23:59:40 +0530

    orufy

--- orufy/src/Components/Navbar/AllRouter.tsx
+++ orufy/src/Components/Navbar/AllRouter.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Home from '../Home/Home'
 import { Routes,Route } from 'react-router-dom'
+import Notification from '../Notification/Notification'
+import Profile from '../Profile/Profile'
+import Category from '../Category/Category'
 
 function AllRouter() {
   return (
     <div>
       <Routes>
             <Route path="/" element={<Home/>}></Route>
-            {/* <Route path="" element={/}></Route>
-            <Route path="" element={/}></Route>
-            <Route path="" element={/}></Route> */}
+            <Route path="/category" element={<Category/>}></Route>
+            <Route path="/notification" element={<Notification />}></Route>
+            <Route path="/profile" element={<Profile/>}></Route>
       </Routes>
     </div>
   )
--- orufy/src/Components/Navbar/Navbar.tsx
+++ orufy/src/Components/Navbar/Navbar.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>
-      <div>
-        {/* <a href="">Logo</a> */}
-            <Link to="/">Logo</Link>
-            <input type="text" placeholder='Search' />
+    <nav className='nav'>
+      <div className='nav1Div'>
+        <Link to="/">
+          <img src="https://i.imgur.com/mKHibaP.png" alt="logo"
+            width={90} /></Link>
+        <div className='inputDiv'>
+          <input className='navInput' type="text" placeholder='Search' />
+          <img src="https://cdn-icons-png.flaticon.com/128/2811/2811806.png" alt="" width={25} />
+        </div>
+
       </div>
-      <div>
-            {/* <Link to="" >Categories</Link> */}
-            {/* <Link to="">bell</Link> */}
-            {/* <Link to="">profile</Link> */}
+      <div className='nav1Div'>
+        <Link style={{
+          display: "flex",
+          alignItems: 'center',
+          gap: '8px'
+        }} to="/category" ><img src="https://cdn-icons-png.flaticon.com/128/1828/1828075.png" alt="" width={30} height={20} /><span>Categories</span> </Link>
+        <Link to="/notification"><svg width="30" height="46" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+        </svg></Link>
+        <Link to="/profile"><img style={{
+          borderRadius: "50%"
+        }} src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=40&h=40&dpr=1" alt="" /></Link>
       </div>
-    </div>
+    </nav>
   )
 }
 
